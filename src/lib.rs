a46f7a664fae1de9844b1f6b74f982a3db37a27c
gk7734 <gamekiller7734@naver.com>
2024-10-31 09:04:58 +0900

    Add Point

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use wasm_bindgen::prelude::*;
+use web_sys::{Element, MouseEvent};
+
+#[wasm_bindgen]
+#[derive(Clone, Debug)]
+pub struct Point {
+    x: f64,
+    y: f64,
+}
+
+#[wasm_bindgen]
+impl Point {
+    #[wasm_bindgen(constructor)]
+    pub fn new (x: f64, y: f64) -> Point {
+        Point { x, y }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+}
