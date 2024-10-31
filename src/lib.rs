36fdc6dd1705920980d16bff0de93c60424870e6
gk7734 <gamekiller7734@naver.com>
2024-10-31 09:27:26 +0900

    Add DraggalbeBlock Struct

--- src/lib.rs
+++ src/lib.rs
@@ -50,3 +50,27 @@ impl Size {
     }
 }
 
+#[wasm_bindgen]
+pub struct DraggableBlock {
+    position: Point,
+    size: Size,
+    min_size: Size,
+    is_dragging: bool,
+    is_resizing: bool,
+    drag_start: Point,
+}
+
+#[wasm_bindgen]
+impl DraggableBlock {
+    #[wasm_bindgen(constructor)]
+    pub fn new(x: f64, y: f64, width: f64, height: f64) -> DraggableBlock {
+        DraggableBlock {
+            position: Point::new(x, y),
+            size: Size::new(width, height),
+            min_size: Size::new(100.0, 40.0),
+            is_dragging: false,
+            is_resizing: false,
+            drag_start: Point::new(0.0, 0.0),
+        }
+    }
+}
